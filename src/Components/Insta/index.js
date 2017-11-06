ee35cc060b21fc8b8f51b0e12682285ce77e7545
BobWassermann <bob@wassermann.nl>
2017-11-06 22:40:19 +0100

    Fixes Instagram API fetch

--- src/Components/Insta/index.js
+++ src/Components/Insta/index.js
@@ -1,16 +1,13 @@
 import React, { Component } from 'react';
-import axios from 'axios';
 import jsonp from 'jsonp';
 
-
 export default class Insta extends Component {
-
   constructor(props) {
     super(props);
 
     this.state = {
       instagramPosts: [],
-      loading: true
+      loading: false
     }
   }
 
@@ -18,45 +15,36 @@ export default class Insta extends Component {
     const user = '240628817';
     const access_token = '240628817.07455da.df0ae2fa48d34e299d33a5e6b62e47a2';
     const api = 'https://api.instagram.com/v1';
-    // axios.get(`${api}/users/${user}/media/recent?access_token=${access_token}&count=4`)
-    //   .then(x => {
-    //     this.setState({
-    //     instagramPosts: x.data.data
-    //     })
-    //     this.setState({
-    //       loading: false
-    //     })
-    //     console.log(x.data.data)
-    //   })
+    const fetch_url = `${api}/users/${user}/media/recent?access_token=${access_token}&count=4`;
 
-    jsonp(`${api}/users/${user}/media/recent?access_token=${access_token}&count=4`, {}, function (err, x) {
-      if (err) {
-        console.error(err.message);
-      } else {
-        console.log(x.data);
-      }
-    });
+    jsonp(fetch_url, {}, (err, data) => {
+      this.setState({
+        instagramPosts: data.data
+      }, () => {
+        this.setState({ loading: true })
+      })
+    })
   }
 
   render() {
     const { title } = this.props
-
-    if (this.state.loading === true) return false
+    const { instagramPosts, loading } = this.state
+    if (loading !== true) return false
     return (
       <div className="insta blocklayout">
         <div className="instaWrapper">
           <h1>{title}</h1>
-          {this.state.instagramPosts.map((post, i) => 
+          {instagramPosts.map((post, i) => (
             <a href={post.link} target='_blank' key={i} className="instaLink">
-              <img className="instaPost" key={i} 
-                  src={post.images.standard_resolution.url} 
+              <img className="instaPost" key={i}
+                  src={post.images.standard_resolution.url}
                   width={post.images.standard_resolution.width}
                   height={post.images.standard_resolution.height}
                   alt={(post.caption === 'null') ? post.caption.text : 'nothing here'}
               />
             </a>
-          )}
-        </div> 
+          ))}
+        </div>
       </div>
     )
   }
